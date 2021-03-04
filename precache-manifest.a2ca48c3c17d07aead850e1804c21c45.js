ebe4b655d382e535db97e0d514efc84a290a0498
varun <varunprakash652@gmail.com>
2021-03-04 22:00:16 +0530

    Updates

---
+++ precache-manifest.a2ca48c3c17d07aead850e1804c21c45.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2bdfe4ca445410373808",
+    "url": "/portfolio/static/css/main.6acbcbe0.chunk.css"
+  },
+  {
+    "revision": "2bdfe4ca445410373808",
+    "url": "/portfolio/static/js/main.2bdfe4ca.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "2318ec0b07ed0a2ba271f627b9f3bf41",
+    "url": "/portfolio/index.html"
+  }
+];
